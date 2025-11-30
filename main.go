07bfa13d30c839db216757c68df95196785a222c
Pete <petehal.dev@gmail.com>
2025-11-30 23:37:22 +0200

    Started main function

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	fmt.Printf("starting crawl of: %v\n", os.Args[1])
 }
